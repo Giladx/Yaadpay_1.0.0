9cdc6cde6726f7d1aa5cc916f29898746b63fb4c
Gilad Levi <gilad.x@gmail.com>
2020-01-24 19:23:57 +0200

    Security & Gui

--- upload/admin/language/en-gb/extension/payment/yaadpay.php
+++ upload/admin/language/en-gb/extension/payment/yaadpay.php
@@ -1,7 +1,7 @@
 <!-- Author : Gilad Levi - https://bytii.cloud --> 
 <!-- github : https://github.com/Giladx/Yaadpay_1.0.0 -->
 <?php
-$version = '3.0.2';
+$version = '7.0.5.0';
 
 // Heading
 $_['heading_title']					= 'Yaadpay '.$version.'';
